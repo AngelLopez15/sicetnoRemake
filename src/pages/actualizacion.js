33684bf9319e1a0f20abe479a1cf5cc348a79f8c
Angel Octavio Lopez <a.o.lopez.cruz@gmail.com>
2021-01-07 20:23:35 -0600

    Navbar esqueleto

--- src/pages/actualizacion.js
+++ src/pages/actualizacion.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from '../components/navbar/Navbar'
 
 export default function actualizacion() {
   return (
     <div>
+      <Navbar />
       <h1 className="bg-red-200">Actualización</h1>
     </div>
   )
