759d8d52b427faf10e615ad0568d1279871d13c8
Angel Octavio Lopez <a.o.lopez.cruz@gmail.com>
2021-02-22 00:34:29 -0600

    Marcadores conflictos color rojo agregados

--- src/components/Mapa.js
+++ src/components/Mapa.js
@@ -1,26 +1,50 @@
 import React from 'react'
-import { MapContainer, TileLayer } from 'react-leaflet'
 import "leaflet/dist/leaflet.css"
+import L from 'leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import all from '../data/all.json'
+
+delete L.Icon.Default.prototype._getIconUrl
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+})
+
+const location = [23.380964227121666, -99.16353656125003]
+const zoom = 5
 
 export default function Mapa() {
   return (
     <>
       {
         (typeof window !== 'undefined')
-        ?
+          ?
           <MapContainer
-            center={[23.380964227121666, -99.16353656125003]} 
-            zoom={5} 
-            scrollWheelZoom={true} 
+            center={location}
+            zoom={zoom}
+            scrollWheelZoom={true}
             style={{ height: 500, width: "100%" }}
           >
-            <TileLayer 
+            <TileLayer
               attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+            {
+              all.map((item, index) => (
+                <Marker key={index} position={item.location}>
+                  <Popup>
+                    <h3>{item.titulo}</h3>
+                    <p>{item.texto}</p>
+                    <a href={item.url} target="_blank" rel="noopener noreferrer">Ver más detalles</a>
+                  </Popup>
+                </Marker>
+              ))
+            }
           </MapContainer>
-        :
-        <p>Cargando Mapa</p>
+          :
+          <p>Cargando Mapa</p>
       }
     </>
   )
--- src/pages/objetivo.js
+++ src/pages/objetivo.js
@@ -5,40 +5,97 @@ export default function objetivo() {
 
   const citas = [
     {
-      cita: '“Base de datos. Sistema de Consulta de Organizaciones Políticas y Conflictos Étnicos en las Américas”. Ponencia. Seminario Permanente Teorías de la Democracia (Centro de Investigaciones Interdisciplinarias en Ciencias y Humanidades, UNAM, febrero de 2012).'
+      cita: '“Base de datos. Sistema de Consulta de Organizaciones Políticas y Conflictos Étnicos en las Américas”. Ponencia. Seminario Permanente Teorías de la Democracia (Centro de Investigaciones Interdisciplinarias en Ciencias y Humanidades, UNAM, febrero de 2012).',
+      bg: 'bg-yellow-200 mx-2 my-2 rounded-2xl shadow-lg',
     },
     {
-      cita: '“Base de datos. Sistema de Consulta de Organizaciones Políticas y Conflictos Étnicos en las Américas”. Conferencia magistral. Seminario Política, pueblos indígenas y medio ambiente (El Colegio de Tlaxcala, San Pablo Apetatitlán, Tlaxcala, mayo de 2012).'
+      cita: '“Base de datos. Sistema de Consulta de Organizaciones Políticas y Conflictos Étnicos en las Américas”. Conferencia magistral. Seminario Política, pueblos indígenas y medio ambiente (El Colegio de Tlaxcala, San Pablo Apetatitlán, Tlaxcala, mayo de 2012).',
+      bg: 'bg-blue-200 mx-2 my-2 rounded-2xl shadow-lg',
     },
     {
-      cita: '“Indigenous peoples and natural resources. The right of consultation”. Conferencia internacional When capitalism meets tradition: Resource extraction and indigenous communities. (McGill University, Canada, abril de 2012).'
+      cita: '“Indigenous peoples and natural resources. The right of consultation”. Conferencia internacional When capitalism meets tradition: Resource extraction and indigenous communities. (McGill University, Canada, abril de 2012).',
+      bg: 'bg-yellow-200 mx-2 my-2 rounded-2xl shadow-lg',
     },
     {
-      cita: '“La cultura política indígena. Una Mirada desde la base de datos de organizaciones políticas de América Latina ORGINDAL” (Universidad Católica de Temuco, Campus San Francisco, Chile, abril de 2012).'
+      cita: '“La cultura política indígena. Una Mirada desde la base de datos de organizaciones políticas de América Latina ORGINDAL” (Universidad Católica de Temuco, Campus San Francisco, Chile, abril de 2012).',
+      bg: 'bg-blue-200 mx-2 my-2 rounded-2xl shadow-lg',
     },
     {
-      cita: '“Indigenous right for consultation and the agenda for inclusive development”. Ponencia. Simposio Comparative Indigeneities of the Americas (Congreso Internacional de Americanistas 54, Viena, Austria, Julio de 2012).'
+      cita: '“Indigenous right for consultation and the agenda for inclusive development”. Ponencia. Simposio Comparative Indigeneities of the Americas (Congreso Internacional de Americanistas 54, Viena, Austria, Julio de 2012).',
+      bg: 'bg-yellow-200 mx-2 my-2 rounded-2xl shadow-lg',
     },
     {
-      cita: '“Base de datos. Sistema de Consulta de Organizaciones Políticas y Conflictos Étnicos en las Américas”. Ponencia. (Comité RC05, 2nd ISA, Forum of Sociology, Social Justice and Democratization, Buenos Aires, agosto de 2012).'
+      cita: '“Base de datos. Sistema de Consulta de Organizaciones Políticas y Conflictos Étnicos en las Américas”. Ponencia. (Comité RC05, 2nd ISA, Forum of Sociology, Social Justice and Democratization, Buenos Aires, agosto de 2012).',
+      bg: 'bg-blue-200 mx-2 my-2 rounded-2xl shadow-lg',
     },
     {
-      cita: '“Base de Datos. ORGINDAL”. Ponencia. Seminario de Antropología Política. Centro de Investigaciones y Estudios Superiores en Antropología Social” (CIESAS, mayo de 2011).'
+      cita: '“Base de Datos. ORGINDAL”. Ponencia. Seminario de Antropología Política. Centro de Investigaciones y Estudios Superiores en Antropología Social” (CIESAS, mayo de 2011).',
+      bg: 'bg-yellow-200 mx-2 my-2 rounded-2xl shadow-lg',
     },
     {
-      cita: '“Cultura política indígena y democracia. ORGINDAL y SICETNO” LXI Legislatura de la Cámara de Diputados del H. Congreso de la Unión y el IISUNAM (Palacio Legislativo, abril de 2011).'
+      cita: '“Cultura política indígena y democracia. ORGINDAL y SICETNO” LXI Legislatura de la Cámara de Diputados del H. Congreso de la Unión y el IISUNAM (Palacio Legislativo, abril de 2011).',
+      bg: 'bg-blue-200 mx-2 my-2 rounded-2xl shadow-lg',
     },
     {
-      cita: '“The Right of Consultation and Natural Resources”. Ponencia. Seminario Internacional. Rethinking Development. Ethics and Social Inclusion (UNESCO, México, D.F., agosto de 2011).'
+      cita: '“The Right of Consultation and Natural Resources”. Ponencia. Seminario Internacional. Rethinking Development. Ethics and Social Inclusion (UNESCO, México, D.F., agosto de 2011).',
+      bg: 'bg-yellow-200 mx-2 my-2 rounded-2xl shadow-lg',
     },
     {
-      cita: '“Multiculturalidad y derechos indígenas en México”. Ponencia. Seminario permanente de investigación del cuerpo académico: Sociedad cultura y política (Universidad de Guanajuato, Campus León, octubre de 2011).'
+      cita: '“Multiculturalidad y derechos indígenas en México”. Ponencia. Seminario permanente de investigación del cuerpo académico: Sociedad cultura y política (Universidad de Guanajuato, Campus León, octubre de 2011).',
+      bg: 'bg-blue-200 mx-2 my-2 rounded-2xl shadow-lg',
     },
     {
-      cita: '“Data bank Orgindal”. Ponencia. Second Conference on Ethnicity, Race and Indigenous Peoples in Latin America and the Caribbean (University of California, San Diego, noviembre de 2011).'
+      cita: '“Data bank Orgindal”. Ponencia. Second Conference on Ethnicity, Race and Indigenous Peoples in Latin America and the Caribbean (University of California, San Diego, noviembre de 2011).',
+      bg: 'bg-yellow-200 mx-2 my-2 rounded-2xl shadow-lg',
     },
     {
-      cita: '“Ethnic conflicts or ethno nationalism. The political activism of indigenous peoples in Latin America. Ponencia. (Brooks World Poverty Institute, University of Manchester, mayo de 2010).'
+      cita: '“Ethnic conflicts or ethno nationalism. The political activism of indigenous peoples in Latin America. Ponencia. (Brooks World Poverty Institute, University of Manchester, mayo de 2010).',
+      bg: 'bg-blue-200 mx-2 my-2 rounded-2xl shadow-lg',
+    },
+  ]
+
+  const atributos = [
+    {
+      icono: '',
+      bg: 'bg-green-100 w-96 p-6 mx-2 my-2 rounded-2xl shadow-lg',
+      titulo: 'Sistema de consulta digital',
+      texto: 'Crear y desarrollar un sistema de consulta digital con información actualizada de las organizaciones de los pueblos indígenas y de los conflictos étnicos que ocurren en las Américas.',
+    },
+    {
+      icono: '',
+      bg: 'bg-blue-100 w-96 p-6 mx-2 my-2 rounded-2xl shadow-lg',
+      titulo: 'Propuestas de modelo de autonomía',
+      texto: 'Ofrecer una plataforma digital en la que se incluyen las propuestas y modelos de autonomía y libre determinación que han diseñado y ejecutan algunos pueblos indios de México, Ecuador y Bolivia.',
+    },
+    {
+      icono: '',
+      bg: 'bg-green-100 w-96 p-6 mx-2 my-2 rounded-2xl shadow-lg',
+      titulo: 'Accesibilidad',
+      texto: 'Construir y desarrollar una plataforma digital que pueda ser accesible a las organizaciones indígenas cuando la técnica de negociación política, tal como el cabildeo o el consenso, sean requeridos en asuntos de interés jurídico indígena.',
+    },
+    {
+      icono: '',
+      bg: 'bg-blue-100 w-96 p-6 mx-2 my-2 rounded-2xl shadow-lg',
+      titulo: 'Actualización',
+      texto: 'Crear una plataforma que posibilite la actualización y retroalimentación de la información sobre acción colectiva y conflictividad indígenas, y que sirva para ampliar su difusión dentro y fuera del país, aprovechando las posibilidades que ofrece la globalización digital de la Internet.',
+    },
+    {
+      icono: '',
+      bg: 'bg-green-100 w-96 p-6 mx-2 my-2 rounded-2xl shadow-lg',
+      titulo: 'Acceso a minorías',
+      texto: 'Ampliar y fortalecer la capacidad política de los pueblos indígenas. La plataforma digital permitirá el acceso de las minorías étnicas a la representación política, al contar con medios de intercomunicación y actualización de información. La representatividad política de minorías se ha convertido en un indicador para evaluar la calidad de la democracia en los sistemas políticos contemporáneos.',
+    },
+    {
+      icono: '',
+      bg: 'bg-green-100 w-96 p-6 mx-2 my-2 rounded-2xl shadow-lg',
+      titulo: 'Acceso a la información',
+      texto: 'En suma, la función de la plataforma es facilitar el acceso a la información y su socialización entre los diferentes actores sociales que involucren intereses étnicos, y de esta manera contribuir a que el trabajo desarrollado en las organizaciones logre ser visible en cuanto a sus planteamientos y propuestas con respecto a la autonomía, a la libre determinación, entre otros. La plataforma también muestra los tipos de conflictividad étnica en las Américas e identifica los daños y amenazas más frecuentes que ponen en riesgo a las poblaciones indígenas y a los territorios que habitan.',
+    },
+    {
+      icono: '',
+      bg: 'bg-blue-100 w-96 p-6 mx-2 my-2 rounded-2xl shadow-lg',
+      titulo: 'Desarrollo de la cultura política indígena',
+      texto: 'Contribuir al desarrollo de la cultura política de los pueblos indígenas por medio de la tecnología que hará posible un acercamiento entre las organizaciones indígenas y el gobierno, con el fin de conocer el diseño y la ejecución del marco jurídico mexicano y su vinculación con las demandas de los pueblos indígenas. El funcionamiento de la plataforma digital permitirá fortalecer las iniciativas de los pueblos originarios en materia de consultas públicas.',
     },
   ]
 
@@ -51,52 +108,32 @@ export default function objetivo() {
         <section className="my-10">
           <h2 className="text-moradoSicetno text-4xl font-bold text-center mb-10">SICETNO</h2>
           <p className="mb-6">SICETNO es una plataforma digital desarrollada con herramientas innovadoras para vincular la investigación en ciencias sociales con las tecnologías de la información sobre temas de democracia y derechos humanos en el continente.</p>
-          <div>
-            <div>
-              <h3>Sistema de consulta digital</h3>
-              <p>Crear y desarrollar un sistema de consulta digital con información actualizada de las organizaciones de los pueblos indígenas y de los conflictos étnicos que ocurren en las Américas.</p>
-            </div>
-            <div>
-              <h3>Propuestas de modelo de autonomía</h3>
-              <p>Ofrecer una plataforma digital en la que se incluyen las propuestas y modelos de autonomía y libre determinación que han diseñado y ejecutan algunos pueblos indios de México, Ecuador y Bolivia.</p>
-            </div>
-            <div>
-              <h3>Accesibilidad</h3>
-              <p>Construir y desarrollar una plataforma digital que pueda ser accesible a las organizaciones indígenas cuando la técnica de negociación política, tal como el cabildeo o el consenso, sean requeridos en asuntos de interés jurídico indígena.</p>
-            </div>
-            <div>
-              <h3>Actualización</h3>
-              <p>Crear una plataforma que posibilite la actualización y retroalimentación de la información sobre acción colectiva y conflictividad indígenas, y que sirva para ampliar su difusión dentro y fuera del país, aprovechando las posibilidades que ofrece la globalización digital de la Internet.</p>
-            </div>
-            <div>
-              <h3>Acceso a minorías</h3>
-              <p>Ampliar y fortalecer la capacidad política de los pueblos indígenas. La plataforma digital permitirá el acceso de las minorías étnicas a la representación política, al contar con medios de intercomunicación y actualización de información. La representatividad política de minorías se ha convertido en un indicador para evaluar la calidad de la democracia en los sistemas políticos contemporáneos.</p>
-            </div>
-            <div>
-              <h3>Desarrollo de la cultura política indígena</h3>
-              <p>Contribuir al desarrollo de la cultura política de los pueblos indígenas por medio de la tecnología que hará posible un acercamiento entre las organizaciones indígenas y el gobierno, con el fin de conocer el diseño y la ejecución del marco jurídico mexicano y su vinculación con las demandas de los pueblos indígenas. El funcionamiento de la plataforma digital permitirá fortalecer las iniciativas de los pueblos originarios en materia de consultas públicas.</p>
-            </div>
-            <div>
-              <h3>Acceso a la información</h3>
-              <p>En suma, la función de la plataforma es facilitar el acceso a la información y su socialización entre los diferentes actores sociales que involucren intereses étnicos, y de esta manera contribuir a que el trabajo desarrollado en las organizaciones logre ser visible en cuanto a sus planteamientos y propuestas con respecto a la autonomía, a la libre determinación, entre otros. La plataforma también muestra los tipos de conflictividad étnica en las Américas e identifica los daños y amenazas más frecuentes que ponen en riesgo a las poblaciones indígenas y a los territorios que habitan.</p>
-            </div>
+          <div className="flex flex-wrap">
+            {
+              atributos.map((item, index) => (
+                  <div className={item.bg} key={index}>
+                    <h3 className="font-bold text-center text-xl mb-2">{item.titulo}</h3>
+                    <p className="text-justify">{item.texto}</p>
+                  </div>
+              ))
+            }
           </div>
         </section>
         <section className="mb-10">
-          <h2 className="text-moradoSicetno text-4xl text-center mb-10">La base cuenta con mapas de conflictos y de las organizaciones de México, Bolivia, Ecuador y Chile</h2>
+          <h2 className="text-moradoSicetno text-4xl font-bold text-center mb-10">La base cuenta con mapas de conflictos y de las organizaciones de México, Bolivia, Ecuador y Chile</h2>
           <div>
             <p>INSERTAR MAPA</p>
           </div>
         </section>
         <section className="mb-10">
-          <h2 className="text-moradoSicetno text-4xl text-center mb-10">Difusión de la plataforma</h2>
+          <h2 className="text-moradoSicetno text-4xl font-bold text-center mb-10">Difusión de la plataforma</h2>
           <p>En el transcurso del proyecto se realizaron conferencias en donde se mencionaron las diversas organizaciones indígenas y los conflictos étnicos que se encuentran en ella y las diferentes herramientas de búsqueda y extracción de datos esto con el fin de dar a conocer la plataforma como una herramienta para estudiantes e interesados en el tema.</p>
-          <div>
+          <div className="flex flex-wrap justify-around mt-5">
             {
-              citas.map((item, index)=>(
-                <div key={index}>
-                  <blockquote>
-                    <p>{item.cita}</p>
+              citas.map((item, index) => (
+                <div className={item.bg} key={index}>
+                  <blockquote className="w-96 px-8 py-8">
+                    <p className="font-semibold">{item.cita}</p>
                   </blockquote>
                 </div>
               ))
