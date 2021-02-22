6107e78054f6cb2acc7a06f1096d4772915d719f
Angel Octavio Lopez <a.o.lopez.cruz@gmail.com>
2021-02-21 18:34:33 -0600

    carrusel con componenete de gatsby

--- src/components/Carrusel.js
+++ src/components/Carrusel.js
@@ -1,12 +1,8 @@
 import React from 'react'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
-// import frase1 from '../images/carrusel/frase1.webp'
-// import frase2 from '../images/carrusel/frase2.webp'
-// import frase3 from '../images/carrusel/frase3.webp'
-// import frase4 from '../images/carrusel/frase4.webp'
-// import frase5 from '../images/carrusel/frase5.webp'
-// import frase6 from '../images/carrusel/frase6.webp'
+import ImageLogos from './ImageLogos';
+
 
 export default function Carrusel(props) {
 
@@ -16,22 +12,28 @@ export default function Carrusel(props) {
   return (
     <Carousel>
       <div>
-        <img src={frase1} alt="Recomendacion 1" />
+        {/* <img src={frase1} alt="Recomendacion 1" /> */}
+        <ImageLogos fileName="frase1.webp" alt="Recomendacion 1" />
       </div>
       <div>
-        <img src={frase2} alt="Recomendacion 2" />
+        {/* <img src={frase2} alt="Recomendacion 2" /> */}
+        <ImageLogos fileName="frase2.webp" alt="Recomendacion 1" />
       </div>
       <div>
-        <img src={frase3} alt="Recomendacion 3" />
+        {/* <img src={frase3} alt="Recomendacion 3" /> */}
+        <ImageLogos fileName="frase3.webp" alt="Recomendacion 1" />
       </div>
       <div>
-        <img src={frase4} alt="Recomendacion 4" />
+        {/* <img src={frase4} alt="Recomendacion 4" /> */}
+        <ImageLogos fileName="frase4.webp" alt="Recomendacion 1" />
       </div>
       <div>
-        <img src={frase5} alt="Recomendacion 5" />
+        {/* <img src={frase5} alt="Recomendacion 5" /> */}
+        <ImageLogos fileName="frase5.webp" alt="Recomendacion 1" />
       </div>
       <div>
-        <img src={frase6} alt="Recomendacion 6" />
+        {/* <img src={frase6} alt="Recomendacion 6" /> */}
+        <ImageLogos fileName="frase6.webp" alt="Recomendacion 1" />
       </div>
     </Carousel>
   )
